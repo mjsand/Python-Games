1afb57291113270f75c3ea07ce985fa53581353a
mjsand <63204640+mjsand@users.noreply.github.com>
2021-02-07 09:21:01 -0800

    Add files via upload

--- randomNumberGuesser.py
+++ randomNumberGuesser.py
@@ -1,15 +1,28 @@
-from random import randrange
 
-n = randrange(1000)
-
-while True:
-    g = int(input('Enter a number: '))
+def randomNumberGame():
+    from random import randrange
     
-    if g == n :
-        print('You Win!')
-        break
-    elif g < n :
-        print ('Try a bigger number.')
-    elif g > n :
-        print('Try a smaller number.')
+    n = randrange(100)
+    count = 0
+    while True:
         
+        try: 
+            guess = int(input('Enter a number: '))
+            count += 1
+        except ValueError:
+            print('Error: Please enter an integer.')
+            continue
+        
+        if guess == n :
+            print('Congratulations! You won in', count, 'guesses.')
+            replay = input('Would you like to play again? Yes or No: ').lower()
+            if replay == 'yes' : randomNumberGame()
+            else: break
+            
+                    
+        elif guess < n :
+            print ('Try a bigger number.')
+        elif guess > n :
+            print('Try a smaller number.')
+            
+randomNumberGame()
